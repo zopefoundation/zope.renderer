e86da226a0672483080caeaa0c86a519dd8a395a
Stephan Richter <stephan.richter@gmail.com>
2007-10-27 18:56:55 +0000

    Get ready for release.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This package provides a framework to convert a string from one format, such as
+Structured or Restructured Text, to another, such as HTML. Converters are
+looked up by adapter and uses other packages to implement the converters.
--- setup.py
+++ setup.py
@@ -15,18 +15,35 @@
 
 $Id$
 """
-
 import os
+from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages, Extension
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.renderer',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.renderer',
-      license='ZPL 2.1',
-      description='Zope renderer',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
+      description='Text Renderer Framework',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 renderer text rest rst stx",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.renderer',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
       namespace_packages=['zope', 'zope.app'],
